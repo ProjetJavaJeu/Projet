0fb08fc664ce6af887e5b1b6fb7f244f3c6c9967
Abridbus <baptiste_vergote@hotmail.com>
2014-11-09 15:55:14 +0100

    Martouf
    Salut

---
+++ src/Martouf/MartoufEstBete.java
@@ -0,0 +1,17 @@
+package Martouf;
+
+public class MartoufEstBete {
+
+	public MartoufEstBete() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
